0a22ed6a9f83e3622a062a03c54965c57de45e3b
nattrio <jirapat.nattrio@gmail.com>
2025-01-20 00:15:34 +0700

    add congrats function and corresponding tests for score messages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use rstest::rstest;
 
 fn main() {
     println!("Hello, world!");
+    let result = congrats(0);
+    println!("{}", result);
 }
 
 pub fn greet(name: &str) -> String {
@@ -11,9 +13,25 @@ pub fn greet(name: &str) -> String {
     }
 }
 
+pub fn congrats(score: u32) -> String {
+    match score {
+        0 => "You scored no points".to_string(),
+        1 => "You scored 1 point".to_string(),
+        _ => format!("You scored {} points", score),
+    }
+}
+
 #[rstest]
 #[case("Alice", "Hello, Alice!".to_string())]
 #[case("", "Hello, world!".to_string())]
 fn test_greet(#[case] input: &str, #[case] expected: String) {
     assert_eq!(expected, greet(input))
+}
+
+#[rstest]
+#[case(0, "You scored no points")]
+#[case(1, "You scored 1 point")]
+#[case(10, "You scored 10 points")]
+fn test_congrats(#[case] input: u32, #[case] expected: String) {
+    assert_eq!(expected, congrats(input))
 }
